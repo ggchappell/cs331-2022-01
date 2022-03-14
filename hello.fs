c44b8d73b3ea87ae55c4cebd196923eaa64767b2
Glenn G. Chappell <ggchappell@alaska.edu>
2022-03-14 15:09:36 -0800

    Add hello.fs

---
+++ hello.fs
@@ -0,0 +1,11 @@
+\ hello.fs
+\ Glenn G. Chappell
+\ 2022-03-14
+\
+\ For CS F331 / CSCE A331 Spring 2022
+\ Hello-world in Forth
+
+
+cr  \ In interactive mode, Forth starts output in the middle of a line
+." Hello, world!" cr
+
