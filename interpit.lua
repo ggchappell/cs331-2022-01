f47c304e2481bc1da9c777a38cd369da84f21c2d
Glenn G. Chappell <ggchappell@alaska.edu>
2022-03-30 11:13:56 -0800

    Add interpit.lua (UNFINISHED)

---
+++ interpit.lua
@@ -0,0 +1,235 @@
+-- interpit.lua  UNFINISHED
+-- Glenn G. Chappell
+-- 2022-03-30
+--
+-- For CS F331 / CSCE A331 Spring 2022
+-- Interpret AST from parseit.parse
+-- Solution to Assignment 6, Exercise 2
+
+
+
+-- *********************************************************************
+-- *                                                                   *
+-- *  DO NOT USE THIS FILE YET! MORE WILL BE ADDED IN THE 3/30 CLASS.  *
+-- *                                                                   *
+-- *********************************************************************
+
+
+
+-- *** To run a Tenrec program, use tenrec.lua, which uses this file.
+
+
+-- *********************************************************************
+-- Module Table Initialization
+-- *********************************************************************
+
+
+local interpit = {}  -- Our module
+
+
+-- *********************************************************************
+-- Symbolic Constants for AST
+-- *********************************************************************
+
+
+local STMT_LIST    = 1
+local PRINT_STMT   = 2
+local RETURN_STMT  = 3
+local ASSN_STMT    = 4
+local FUNC_CALL    = 5
+local FUNC_DEF     = 6
+local IF_STMT      = 7
+local WHILE_LOOP   = 8
+local STRLIT_OUT   = 9
+local CR_OUT       = 10
+local CHAR_CALL    = 11
+local BIN_OP       = 12
+local UN_OP        = 13
+local NUMLIT_VAL   = 14
+local BOOLLIT_VAL  = 15
+local READ_CALL    = 16
+local SIMPLE_VAR   = 17
+local ARRAY_VAR    = 18
+
+
+-- *********************************************************************
+-- Utility Functions
+-- *********************************************************************
+
+
+-- numToInt
+-- Given a number, return the number rounded toward zero.
+local function numToInt(n)
+    assert(type(n) == "number")
+
+    if n >= 0 then
+        return math.floor(n)
+    else
+        return math.ceil(n)
+    end
+end
+
+
+-- strToNum
+-- Given a string, attempt to interpret it as an integer. If this
+-- succeeds, return the integer. Otherwise, return 0.
+local function strToNum(s)
+    assert(type(s) == "string")
+
+    -- Try to do string -> number conversion; make protected call
+    -- (pcall), so we can handle errors.
+    local success, value = pcall(function() return tonumber(s) end)
+
+    -- Return integer value, or 0 on error.
+    if success then
+        if value == nil then
+            return 0
+        else
+            return numToInt(value)
+        end
+    else
+        return 0
+    end
+end
+
+
+-- numToStr
+-- Given a number, return its string form.
+local function numToStr(n)
+    assert(type(n) == "number")
+
+    return tostring(n)
+end
+
+
+-- boolToInt
+-- Given a boolean, return 1 if it is true, 0 if it is false.
+local function boolToInt(b)
+    assert(type(b) == "boolean")
+
+    if b then
+        return 1
+    else
+        return 0
+    end
+end
+
+
+-- astToStr
+-- Given an AST, produce a string holding the AST in (roughly) Lua form,
+-- with numbers replaced by names of symbolic constants used in parseit.
+-- A table is assumed to represent an array.
+-- See the Assignment 4 description for the AST Specification.
+--
+-- THIS FUNCTION IS INTENDED FOR USE IN DEBUGGING ONLY!
+-- IT SHOULD NOT BE CALLED IN THE FINAL VERSION OF THE CODE.
+function astToStr(x)
+    local symbolNames = {
+        "STMT_LIST", "PRINT_STMT", "RETURN_STMT", "ASSN_STMT",
+        "FUNC_CALL", "FUNC_DEF", "IF_STMT", "WHILE_LOOP", "STRLIT_OUT",
+        "CR_OUT", "CHAR_CALL", "BIN_OP", "UN_OP", "NUMLIT_VAL",
+        "BOOLLIT_VAL", "READ_CALL", "SIMPLE_VAR", "ARRAY_VAR",
+    }
+    if type(x) == "number" then
+        local name = symbolNames[x]
+        if name == nil then
+            return "<Unknown numerical constant: "..x..">"
+        else
+            return name
+        end
+    elseif type(x) == "string" then
+        return '"'..x..'"'
+    elseif type(x) == "boolean" then
+        if x then
+            return "true"
+        else
+            return "false"
+        end
+    elseif type(x) == "table" then
+        local first = true
+        local result = "{"
+        for k = 1, #x do
+            if not first then
+                result = result .. ","
+            end
+            result = result .. astToStr(x[k])
+            first = false
+        end
+        result = result .. "}"
+        return result
+    elseif type(x) == "nil" then
+        return "nil"
+    else
+        return "<"..type(x)..">"
+    end
+end
+
+
+-- *********************************************************************
+-- Primary Function for Client Code
+-- *********************************************************************
+
+
+-- interp
+-- Interpreter, given AST returned by parseit.parse.
+-- Parameters:
+--   ast     - AST constructed by parseit.parse
+--   state   - Table holding Tenrec variables & functions
+--             - AST for function xyz is in state.f["xyz"]
+--             - Value of simple variable xyz is in state.v["xyz"]
+--             - Value of array item xyz[42] is in state.a["xyz"][42]
+--   incall  - Function to call for line input
+--             - incall() inputs line, returns string with no newline
+--   outcall - Function to call for string output
+--             - outcall(str) outputs str with no added newline
+--             - To print a newline, do outcall("\n")
+-- Return Value:
+--   state, updated with changed variable values
+function interpit.interp(ast, state, incall, outcall)
+    -- Each local interpretation function is given the AST for the
+    -- portion of the code it is interpreting. The function-wide
+    -- versions of state, incall, and outcall may be used. The
+    -- function-wide version of state may be modified as appropriate.
+
+
+    -- Forward declare local functions
+    local interp_stmt_list
+    local interp_stmt
+    local eval_expr
+
+
+    -- interp_stmt_list
+    -- Given the ast for a statement list, execute it.
+    function interp_stmt_list(ast)
+        print("*** UNIMPLEMENTED STATEMENT LIST")
+    end
+
+
+    -- interp_stmt
+    -- Given the ast for a statement, execute it.
+    function interp_stmt(ast)
+        print("*** UNIMPLEMENTED STATEMENT")
+    end
+
+
+    -- eval_expr
+    -- Given the AST for an expression, evaluate it and return the
+    -- value.
+    function eval_expr(ast)
+        print("*** UNIMPLEMENTED EXPRESSION")
+    end
+
+
+    -- Body of function interp
+    interp_stmt_list(ast)
+    return state
+end
+
+
+-- *********************************************************************
+-- Module Table Return
+-- *********************************************************************
+
+
+return interpit
+
